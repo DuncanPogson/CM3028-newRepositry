2b4500c2692b27ccb37107934bad3d3ab1a17a48
Dannyrf1995 <dannyrf1995@gmail.com>
2016-12-11 02:55:24 +0000

    Buttons on calendar #3

--- header.php
+++ header.php
@@ -13,7 +13,7 @@ session_start();
     <meta charset="UTF-8">
     <title>Go Portlethen</title>
     <link rel="stylesheet" href="calendarCss.css" type="text/css" media="all"/>
-    <!-- <script language="JavaScript" type="text/javascript">
+    <script language="JavaScript" type="text/javascript">
         function initialCalendar() {
             var hr = new XMLHttpRequest();
             var url = "calendar_start.php";
@@ -83,7 +83,7 @@ session_start();
             document.getElementById("showCalendar").innerHTML = "processing...";
         }
     </script>
-     -->
+
 </head>
 <body onLoad="initialCalendar();">
 <div id="showCalendar"></div>
