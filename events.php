1a972b4b7a8ff944b171e8965864695e58d5cd9a
1304076 <dannyrf1995@gmail.com>
2016-12-12 17:30:32 +0000

    events and calendar changes

--- events.php
+++ events.php
@@ -56,7 +56,7 @@ while($row = $result->fetch_array()) {
 }
 
         if (mysqli_query($conn, $sql)) {
-//            header("location:home.php");
+            header("location:home.php");
         } else {
             echo "Error: " . $sql . "<br>" . mysqli_error($conn);
             echo "cannot create club, please try again later.";
