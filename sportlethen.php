7b61a9d38aa20a0a1d74c2dc5b57be7cfa106626
duncanpogson <duncanpogson@gmail.com>
2016-12-12 02:14:46 +0000

    Allow Admins to set users access levels

--- sportlethen.php
+++ sportlethen.php
@@ -12,6 +12,13 @@ session_start();
 include ("header.php");
 include ("Database/LoginSystem/DB_Connect.php");
 include ("calendar_start.php");
+
+if (isset($_SESSION['login_username'])) {
+    if (((int)$_SESSION['AccessLevel']) >= 4) {
+        echo "<li><a href='setAdmins.php'>Set Club Admins</a></li>";
+    }
+}
+
 echo "
 
 <main>
@@ -25,12 +32,6 @@ Our website is a single access point to find out more about the fantastic sporti
 which gave people the opportunity to take part in taster sessions of the different sports they can take up in the local area. </p>
 
 ";
-if (isset($_SESSION['login_username'])) {
-    if (((int)$_SESSION['AccessLevel']) >= 4) {
-        echo "<li><a href='setAdmins.php'>Set Club Admins</a></li>";
-    }
-}
-
 
 $sql = "SELECT * FROM club";
 $result = $conn->query($sql);
